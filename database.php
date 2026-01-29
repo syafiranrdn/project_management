a8d352812ddf8a55ffcac6feb3a32d6ddb0d8015
syafiranrdn <154555904+syafiranrdn@users.noreply.github.com>
2026-01-29 18:24:42 +0800

    Update database.php

--- database.php
+++ database.php
@@ -7,8 +7,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-header("Content-Type: application/json");
-
 /* =============================
    READ RAILWAY ENV VARIABLES
 ============================= */
