606bad1dc3bb5854a462e8137a4d3a3c389940a5
syafiranrdn <154555904+syafiranrdn@users.noreply.github.com>
2026-01-29 22:02:01 +0800

    Update list.php

--- users_api/list.php
+++ users_api/list.php
@@ -3,24 +3,24 @@ header("Content-Type: application/json");
 
 require_once __DIR__ . '/../database.php';
 
-$sql = "
-SELECT
-    u.user_id,
-    u.name,
-    u.email,
-    u.role,
-    u.admin_level,
-    u.department_id,
-    d.department_name,
-    u.status,
-    u.created_at
-FROM users u
-LEFT JOIN departments d ON d.department_id = u.department_id
-ORDER BY u.user_id ASC
-";
-
 try {
-    $stmt = $pdo->query($sql);
+    $stmt = $pdo->query("
+        SELECT
+            u.user_id,
+            u.name,
+            u.email,
+            u.role,
+            u.admin_level,
+            u.department_id,
+            d.department_name,
+            u.status,
+            u.created_at
+        FROM users u
+        LEFT JOIN departments d
+            ON d.department_id = u.department_id
+        ORDER BY u.user_id ASC
+    ");
+
     $data = $stmt->fetchAll();
 
     echo json_encode([
