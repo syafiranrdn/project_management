e278511da516f32c6b8b69dc186d50be989e520a
syafiranrdn <154555904+syafiranrdn@users.noreply.github.com>
2026-01-30 13:07:20 +0800

    Update get_activities.php

--- public/api/get_activities.php
+++ public/api/get_activities.php
@@ -1,18 +1,19 @@
 <?php
-header('Content-Type: application/json');
-require_once __DIR__ . '/../../database.php';
+header('Content-Type: application/json; charset=UTF-8');
 
+require_once __DIR__ . '/../../database.php';
 
 $sql = "SELECT * FROM activities ORDER BY created_at DESC LIMIT 10";
 $result = mysqli_query($conn, $sql);
 
 $data = [];
-while ($row = mysqli_fetch_assoc($result)) {
-    $data[] = $row;
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
 }
 
 echo json_encode([
     "success" => true,
     "data" => $data
 ]);
-
