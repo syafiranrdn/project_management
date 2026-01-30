4effb43c0c076d2535854ce796ac93b60d1605f5
syafiranrdn <154555904+syafiranrdn@users.noreply.github.com>
2026-01-30 10:16:53 +0800

    Update list.php

--- api/calendar_api/list.php
+++ api/calendar_api/list.php
@@ -4,19 +4,15 @@ require_once __DIR__ . '/../../database.php';
 
 $stmt = $conn->query("
     SELECT 
-        user_id,
-        name,
-        email,
-        role,
-        status,
-        created_at
-    FROM users
-    ORDER BY user_id ASC
+        event_id,
+        title,
+        start_date,
+        end_date
+    FROM calendar_events
+    ORDER BY start_date ASC
 ");
-
-$data = $stmt->fetchAll();
 
 echo json_encode([
     "ok" => true,
-    "data" => $data
+    "data" => $stmt->fetchAll()
 ]);
